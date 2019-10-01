7ddc75d12353b8f03bc1cb1f996e13664f9ab41f
Matt <mstewz3646@gmail.com>
2019-09-30 22:54:41 -0700

    just added the objects and properties. Playing catch up.

---
+++ app.js
@@ -0,0 +1,36 @@
+var Seattle = {
+    min: 23,
+    max: 65,
+    avg: 6.3,
+}
+
+var Tokyo = {
+    min: 3,
+    max: 25,
+    avg: 1.2
+}
+
+var Dubai = {
+    min: 11,
+    max: 38,
+    avg: 3.7
+}
+
+var Paris = {
+    min: 20,
+    max: 38,
+    avg: 2.3,
+}
+
+var Lima = {
+    min: 2,
+    max: 16,
+    avg: 4.6
+}
+
+
+// function randomCust(min, max) {
+//     return (Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+//   }
+  
+//   console.log(randomCust(23, 65));
